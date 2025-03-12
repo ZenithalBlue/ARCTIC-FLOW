d1b714fcc38a342dc54ba1a3261fb03437cd25f7
Aqeel Piracha <ap@ap.local>
2025-03-12 12:38:28 +0100

    more detailed code comments, and more descriptive variable names

--- simple_dflux.py
+++ simple_dflux.py
@@ -33,53 +33,60 @@ TODO                   -spatial_res_type, type of spatial grid [DEG | KM]
 
     Notes
     -----
-    ``derivative_degrees2meters`` computes first order derivative of an ocean scalar, using
+    ``derivative_degrees2meters`` computes first order derivatives of an ocean scalar, using
     the FTCS (Forward time-centered space) differncing scheme. Deals with irregular grid sizes by 
     calculating latitudinal distances at each latitude (instead of assigning a constant distance)
-    and so when calculating the longitude distances these values are used
+    and the same is done with the longitudes (also the latitudes are taken into account to calculate
+    longitudinal distances).
     """
-    dtime, dlat, dlon = np.gradient(ds[kwargs['scalar']])
+    # checking if the time dimension is of size 2 
+    if ds.sizes[kwargs['time']] > 2:
+        print('the size of the time dimension should be 2/n')
+        exit
+    derivative_time, derivative_latitude, derivative_longitude = np.gradient(ds[kwargs['scalar']])
     if kwargs['time_resolution'] == 'D':
-        dtime = dtime[0] / (86400)
+        derivative_time = derivative_time[0] / (86400)
     elif kwargs['time_resolution'] == 'W':
-        dtime = dtime[0] / (7 * 86400)
+        derivative_time = derivative_time[0] / (7 * 86400)
     elif kwargs['time_resolution'] == 'M':
-        dtime = dtime[0] / (kwargs['days_in_month'] * 86400)
+        derivative_time = derivative_time[0] / (kwargs['days_in_month'] * 86400)
 
 
     if kwargs['spatial_resolution'] == 'DEG':
         earth_radius = 6370 * 1000
         lat_degree_distance = (2 * np.pi * earth_radius) / 360
 
-        res = np.gradient(ds[kwargs['lat']])
-        distance_lat = lat_degree_distance * res
+        distance_lat = np.gradient(ds[kwargs['lat']]) * lat_degree_distance 
         lon_grid, lat_grid = np.meshgrid(ds[kwargs['lon']], ds[kwargs['lat']])
+        # the distances of each longitude in meters (without the latitudinal dependence)
         res_lon_meters = np.abs(np.gradient(lon_grid)[1] * lat_degree_distance)
+        # ...and with taking the latitudinal dependence into account
         distance_lon = res_lon_meters * np.cos(np.radians(lat_grid))
-
-        dlon = np.nanmean(dlon, 0) 
-        dlat = np.nanmean(dlat, 0) 
+        # averaging over 2 timesteps (results in the same time grid as `derivative_time`)
+        derivative_longitude = np.nanmean(derivative_longitude, 0) 
+        derivative_latitude = np.nanmean(derivative_latitude, 0) 
+        # looping through all lats and longitudes and dividing by their respective grid distances
         for lat in range(ds.sizes[kwargs['lat']]):
-            dlon[lat, :] = dlon[lat, :] / (2*distance_lon[lat, :])
+            derivative_longitude[lat, :] = derivative_longitude[lat, :] / (2*distance_lon[lat, :])
         for lon in range(ds.sizes[kwargs['lon']]):
-            dlat[:, lon] = dlat[:, lon] / (2 * distance_lat)
+            derivative_latitude[:, lon] = derivative_latitude[:, lon] / (2 * distance_lat)
     elif kwargs['spatial_resolution'] == 'Km':
-        res = np.gradient(ds[kwargs['lat']])
-        distance_lat = res * 1000
-        res = np.gradient(ds[kwargs['lat']]) * 1000
+        # here, the user specifies that the resolution is already in km (in the netcdf file)
+        # the rest of the operations are the same 
+        res_lat_meters = np.gradient(ds[kwargs['lat']]) * lat_degree_distance 
         lat_grid, lon_grid = np.meshgrid(ds[kwargs['lon']], ds[kwargs['lat']])
         res_lon_meters = np.gradient(lon_grid)[0] * 1000
         distance_lon = res_lon_meters * np.cos(np.radians(lat_grid))
 
-        dlon = np.nanmean(dlon, 0) 
-        dlat = np.nanmean(dlat, 0) 
+        derivative_longitude = np.nanmean(derivative_longitude, 0) 
+        derivative_latitude = np.nanmean(derivative_latitude, 0) 
         for lat in range(ds.sizes[kwargs['lat']]):
-            dlon[lat, :] = dlon[lat, :] / distance_lon[lat, :]
+            derivative_longitude[lat, :] = derivative_longitude[lat, :] / distance_lon[lat, :]
         for lon in range(ds.sizes[kwargs['lon']]):
-            dlat[:, lon] = dlat[:, lon] / distance_lat
+            derivative_latitude[:, lon] = derivative_latitude[:, lon] / distance_lat
 
 
-    return dtime, dlat, dlon
+    return derivative_time, derivative_latitude, derivative_longitude
 
 def calculate_material_derivative(ds, kwargs):
     """
@@ -100,7 +107,7 @@ def calculate_material_derivative(ds, kwargs):
                    -lon, name of the longitude dimension,
                    -scalar, name of the variable to be derived (FOR CALL TO DERIVATIVE_DEG2M),
                    -time_res, temporal resolution of variable [dAILY | mONTHLY]
-TODO                   -spatial_res_type, type of spatial grid [DEG | KM]
+                   -spatial_res_type, type of spatial grid [DEG | KM]
 
     Returns
     -------
@@ -109,18 +116,24 @@ TODO                   -spatial_res_type, type of spatial grid [DEG | KM]
     timesteps of the original input data
 
     Notes
-    -----
+    ----
     ``calculate_material_derivative`` computes the material derivative of an ocean scalar, using
     the FTCS (Forward time-centered space) differncing scheme (via a call to 
     ``derivative_degrees2meters``) 
     """
-    dtime, dlat, dlon = derivative_degrees2meters(ds, kwargs)
-    temporal_derivative = dtime
-
+    # checking if the time dimension is of size 2 
+    if ds.sizes[kwargs['time']] > 2:
+        print('the size of the time dimension should be 2/n')
+        exit
+    derivative_time, derivative_latitude, derivative_longitude = derivative_degrees2meters(ds, kwargs)
+    '''
+    averaging the sea surface currents over 2 timesteps (i.e. keeping the same temporal grid)
+    ''' 
     mean_u = np.nanmean(ds[kwargs['u']].data, 0) 
     mean_v = np.nanmean(ds[kwargs['v']].data, 0) 
-    advective_derivative = dlon * mean_u + dlat * mean_v
-    material_derivative = temporal_derivative + advective_derivative
+
+    advective_derivative = derivative_longitude * mean_u + derivative_latitude * mean_v
+    material_derivative = derivative_time + advective_derivative
 
     return material_derivative
 
@@ -145,8 +158,7 @@ def calculate_flux(ds, kwargs):
                    -lon, name of the longitude dimension,
                    -scalar, name of the variable to be derived (FOR CALL TO DERIVATIVE_DEG2M),
                    -time_res, temporal resolution of variable [dAILY | mONTHLY]
-TODO                   -spatial_res_type, type of spatial grid [DEG | KM]
-TODO                   -to_netcdf, BOOL TYPE FOR OUTPUT AS A COMPRESSED NETcdf FILE}
+                   -spatial_res_type, type of spatial grid [DEG | KM]
 
     Returns
     -------
@@ -161,6 +173,10 @@ TODO                   -to_netcdf, BOOL TYPE FOR OUTPUT AS A COMPRESSED NETcdf F
     The specific differencing scheme is FTCS (Forward time-centered space) and is done with a 
     call to ``derivative_deg2m`` (through ``calculate_material_derivative``). 
     """
+    # checking if the time dimension is of size 2 
+    if ds.sizes[kwargs['time']] > 2:
+        print('the size of the time dimension should be 2/n')
+        exit
     material_derivative = calculate_material_derivative(ds, kwargs)
 
     mean_mld = np.nanmean(ds[kwargs['mld']].data, 0) 
@@ -188,25 +204,40 @@ def calculate_density_flux(ds, kwargs):
 
     Returns
     -------
-    xarray Dataset : four new entries into `ds` being, 
-                   dflux_sss/sst, haline/thermal component of the density flux
-                   dflux, the density flux
-                   flux_sss/sst, the freshwater/heat flux 
-    file netCDF : optionally saves the populated xarray Dataset as a compressed netCDF
+    3 numpy matrices:
+        -dflux = the net density flux (the sum of the thermal and haline contributions)
+        -dflux_sst = the thermal component of density flux 
+        -dflux_sss = the haline component of density flux
+
 
     Notes
     -----
     before calling this function, you should make a prior call to ``calculate_flux``
-    density_flux computes the density flux through the sea surface as the product between the 
-    mixed layer depth and the material derivative of sea surface density. Additionally, 
-    ``calculate_density_flux`` estimates the thermal and haline components of the density flux.
+    density_flux computes the respective thermal and haline components of density flux 
+    through the sea surface as the product between the mixed layer depth and the material 
+    derivative of sea surface salinity and temperature. Additionally, the density flux is a simple
+    addition of these thermal and haline components.  ``calculate_density_flux`` estimates the 
+    thermal and haline components of the density flux. requires the input data to have a time 
+    dimension of size 2 (as the function averages data over 2 timesteps to avoid temporal grid
+    mismatches with the material derivative)
     """
+    '''
+    calculating:
+        rho = sea surface density
+        alpha = thermal expansion coefficient (i.e. how temperature changes surface density)
+        beta = haline contraction coefficient (i.e. how salinity changes surface density)
+    '''
+    # checking if the time dimension is of size 2 
+    if ds.sizes[kwargs['time']] > 2:
+        print('the size of the time dimension should be 2/n')
+        exit
     rho, alpha, beta = gsw.rho_alpha_beta(ds[kwargs['sss']].data, ds[kwargs['sst']].data, 0)
-
+    # averaging over 2 timesteps to avoid mismatched temporal grids
     mean_rho = np.nanmean(rho, 0) 
     mean_alpha = np.nanmean(alpha, 0) 
     mean_beta = np.nanmean(beta, 0) 
 
+    # the components of the density flux and their sum-the density flux 
     dflux_sss = mean_rho * mean_beta * kwargs['flux_sss']
     dflux_sst = -(mean_rho * mean_alpha * kwargs['flux_sst'])
     dflux = dflux_sst + dflux_sss
@@ -236,15 +267,25 @@ def calculate_all_fluxes(ds, kwargs):
                    -lat, name of the latitude dimension,
                    -lon, name of the longitude dimension,
                    -time_res, temporal resolution of variable [dAILY | mONTHLY]
-TODO                   -spatial_res_type, type of spatial grid [DEG | KM]
+                   -spatial_res_type, type of spatial grid [DEG | KM]
 
     Returns
     -------
-    5 numpy matrices : 1, freshwater flux
-                       2, heat flux
-                       3, density flux
-                       4, density flux (thermal component)
-                       5, density flux (haline component)
+    xarray_dataset_object ds : An xarray Dataset that includes the following variables with
+                               the same dimensions as the input ds, although, with one less
+                               time dimension (due to derivatives)
+                               the variables are:
+                       - flux_sss = freshwater flux (i.e. the product between the material 
+                       derivative of surface salinity and mld)
+                       - flux_sst = heat flux (i.e. the product between the material 
+                       derivative of surface temperature and mld)
+                       - flux_ssd = density flux (i.e. the product between the material 
+                       derivative of surface density and mld)
+                       - dflux_sst = thermal density flux  
+                       - dflux_sss = haline density flux 
+                       - dflux = the sum between the thermal and haline density fluxes (i.e. the 
+                       density flux assuming a linear realtion between surface salinity/temperature
+                       on surface density)
 
     Notes
     -----
@@ -252,8 +293,7 @@ TODO                   -spatial_res_type, type of spatial grid [DEG | KM]
     as the products between the respective material derivatives of the scalars and a mixed layer 
     depth. It uses the FTCS (Forward time-centered space) differncing scheme to compute the 
     the respective derivatives of time, latitude and longitude. The outputs are referenced to 
-    a time grid which is 1/2 forward shifted (relative to the input), although the dimension length
-    is the same as the input (using cyclical boundary conditions)
+    a time grid which is 1/2 forward shifted (relative to the input) 
     """
     # adding ssd to the dataset to compute its fluxes alone 
     ssd = gsw.rho(ds[kwargs['sss']].data, ds[kwargs['sst']].data, 0)
@@ -281,11 +321,9 @@ TODO                   -spatial_res_type, type of spatial grid [DEG | KM]
 
         ds = ds_original.isel({'time': [timestep, timestep + 1]})
 
-        #TODO: do the same for density
         kwargs['scalar'] = 'ssd'
         flux_ssd[timestep, :, :], material_derivative_ssd[timestep, :, :] =\
                 calculate_flux(ds, kwargs)
-        #TODO: we want mld division done later
         kwargs['scalar'] = kwargs['sss']
         flux_sss[timestep, :, :], material_derivative_sss[timestep, :, :] =\
                 calculate_flux(ds, kwargs)
@@ -303,17 +341,17 @@ TODO                   -spatial_res_type, type of spatial grid [DEG | KM]
     time_shifted = xr.concat([first_time_halfshift, rest_time_halfshift], dim = 'time')
 
     ds = xr.Dataset({
-        "material_derivative_ssd": (["time", "lat", "lon"], material_derivative_ssd),
-        "material_derivative_sss": (["time", "lat", "lon"], material_derivative_sss),
-        "material_derivative_sst": (["time", "lat", "lon"], material_derivative_sst),
-        "flux_ssd": (["time", "lat", "lon"], flux_ssd),
-        "flux_sss": (["time", "lat", "lon"], flux_sss),
-        "flux_sst": (["time", "lat", "lon"], flux_sst),
-        "dflux": (["time", "lat", "lon"], dflux),
-        "dflux_sss": (["time", "lat", "lon"], dflux_sss),
-        "dflux_sst": (["time", "lat", "lon"], dflux_sst),
-    }, coords={"time": time_shifted, "lat": ds_original[kwargs['lat']], 
-               "lon": ds_original[kwargs['lon']]})
+        "material_derivative_ssd": ([kwargs['time'], kwargs['lat'], kwargs['lon']], material_derivative_ssd),
+        "material_derivative_sss": ([kwargs['time'], kwargs['lat'], kwargs['lon']], material_derivative_sss),
+        "material_derivative_sst": ([kwargs['time'], kwargs['lat'], kwargs['lon']], material_derivative_sst),
+        "flux_ssd": ([kwargs['time'], kwargs['lat'], kwargs['lon']], flux_ssd),
+        "flux_sss": ([kwargs['time'], kwargs['lat'], kwargs['lon']], flux_sss),
+        "flux_sst": ([kwargs['time'], kwargs['lat'], kwargs['lon']], flux_sst),
+        "dflux": ([kwargs['time'], kwargs['lat'], kwargs['lon']], dflux),
+        "dflux_sss": ([kwargs['time'], kwargs['lat'], kwargs['lon']], dflux_sss),
+        "dflux_sst": ([kwargs['time'], kwargs['lat'], kwargs['lon']], dflux_sst),
+    }, coords={kwargs['time']: time_shifted, kwargs['lat']: ds_original[kwargs['lat']], 
+               kwargs['lon']: ds_original[kwargs['lon']]})
 
     if kwargs['to_netcdf']:
         filename = "fluxes.nc"
